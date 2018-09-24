5aec52747a4c9c3d3beff95fe9899679e634b288
gas21 <hans@berea.edu>
2018-09-24 01:00:17 -0400

    a

--- canonical/canonical.py
+++ canonical/canonical.py
@@ -1,16 +1,28 @@
 # use python 3
+import sys
 
 
 def greedy(n, l):
-    pass
+    a, i = 0, 0
+    while n != 0:
+        b = n // l[i]
+        a += b if b > 0 else 0
+        n -= b * l[i] if b > 0 else 0
+        i += 1
+    return a
 
 
-def dynamic(n, l):
+def dynamic(m, l):
+    a = []
+    for i in range(m):
+        
+
+def canonical(n, l):
 
     pass
 
 
 if __name__ == "__main__":
-    m = int(sys.stdin.read())
-    print(m)
-    pass
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    canonical(data[0], reversed(data[1:]))
